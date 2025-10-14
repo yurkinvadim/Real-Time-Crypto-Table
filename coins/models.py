2d58137e9588806f1b8df9179d82223c07aa332f
vadim <yurkinvadim99@gmail.com>
2025-10-14 22:43:13 +0200

    add model to keep coins

--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,21 @@
+from decimal import Decimal
+
 from django.db import models
 
-# Create your models here.
+
+class Coin(models.Model):
+    name = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=50)
+    price = models.DecimalField(
+        max_digits=20,
+        decimal_places=8,
+        default=Decimal('0.0'),
+    )
+    rank = models.IntegerField(default=0, blank=True)
+    image = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        ordering = ['rank']
